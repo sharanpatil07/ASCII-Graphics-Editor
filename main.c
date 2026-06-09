05288a35f99546a7473e8315be853348ddd9b71f
Sinchana N Poojar <sinchananpoojar@gmail.com>
2026-06-09 11:57:25 +0530

    Added rectangle line triangle circle modify features

--- main.c
+++ main.c
@@ -6,77 +6,124 @@
 char canvas[ROWS][COLS];
 
 void initializeCanvas() {
-    for(int i=0;i<ROWS;i++) {
-        for(int j=0;j<COLS;j++) {
-            canvas[i][j] = '_';
+    int i, j;
+    for(i = 0; i < ROWS; i++) {
+        for(j = 0; j < COLS; j++) {
+            canvas[i][j] = ' ';
         }
     }
 }
 
 void displayCanvas() {
-    for(int i=0;i<ROWS;i++) {
-        for(int j=0;j<COLS;j++) {
-            printf("%c ", canvas[i][j]);
+    int i, j;
+
+    for(i = 0; i < ROWS; i++) {
+        for(j = 0; j < COLS; j++) {
+            printf("%c", canvas[i][j]);
         }
         printf("\n");
     }
 }
 
 void drawRectangle(int x, int y, int width, int height) {
-    for(int i=y;i<y+height && i<ROWS;i++) {
-        for(int j=x;j<x+width && j<COLS;j++) {
+    int i, j;
+
+    for(i = y; i < y + height; i++) {
+        for(j = x; j < x + width; j++) {
             canvas[i][j] = '*';
         }
     }
 }
+
 void drawLine(int x1, int y1, int x2, int y2) {
-    if(y1 == y2) { // Horizontal line
-        for(int i = x1; i <= x2; i++) {
+    int i;
+
+    if(y1 == y2) {
+        for(i = x1; i <= x2; i++) {
             canvas[y1][i] = '*';
         }
     }
-    else if(x1 == x2) { // Vertical line
-        for(int i = y1; i <= y2; i++) {
-            canvas[i][x1] = '*';
-        }
-    }
+}
+
+void drawTriangle() {
+    canvas[2][10] = '*';
+
+    canvas[3][9] = '*';
+    canvas[3][11] = '*';
+
+    canvas[4][8] = '*';
+    canvas[4][9] = '*';
+    canvas[4][10] = '*';
+    canvas[4][11] = '*';
+    canvas[4][12] = '*';
+}
+
+void drawCircle() {
+    canvas[7][10] = '*';
+    canvas[6][9] = '*';
+    canvas[6][11] = '*';
+    canvas[8][9] = '*';
+    canvas[8][11] = '*';
+    canvas[7][8] = '*';
+    canvas[7][12] = '*';
+}
+
+void modifyRectangle() {
+    drawRectangle(15, 5, 8, 3);
 }
 
 int main() {
+
     int choice;
+
     initializeCanvas();
 
     while(1) {
-       printf("\n===== 2D Graphics Editor =====\n");
-printf("1. Draw Rectangle\n");
-printf("2. Draw Line\n");
-printf("3. Display Canvas\n");
-printf("4. Clear Canvas\n");
-printf("5. Exit\n");
 
-printf("Enter choice: ");
-scanf("%d", &choice);
+        printf("\n===== 2D Graphics Editor =====\n");
+        printf("1. Draw Rectangle\n");
+        printf("2. Draw Line\n");
+        printf("3. Draw Triangle\n");
+        printf("4. Draw Circle\n");
+        printf("5. Display Canvas\n");
+        printf("6. Clear Canvas\n");
+        printf("7. Modify Rectangle\n");
+        printf("8. Exit\n");
 
-if(choice == 1) {
-    drawRectangle(5, 5, 10, 4);
-}
-else if(choice == 2) {
-    drawLine(2, 2, 15, 2);
-}
-else if(choice == 3) {
-    displayCanvas();
-}
-else if(choice == 4) {
-    initializeCanvas();
-    printf("Canvas Cleared!\n");
-}
-else if(choice == 5) {
-    printf("Exiting...\n");
-    break;
-}
-else {
-    printf("Invalid Choice!\n");
-}
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        if(choice == 1) {
+            drawRectangle(5, 5, 10, 4);
+        }
+        else if(choice == 2) {
+            drawLine(2, 2, 15, 2);
+        }
+        else if(choice == 3) {
+            drawTriangle();
+        }
+        else if(choice == 4) {
+            drawCircle();
+        }
+        else if(choice == 5) {
+            displayCanvas();
+        }
+        else if(choice == 6) {
+            initializeCanvas();
+            printf("Canvas Cleared!\n");
+        }
+        else if(choice == 7) {
+            modifyRectangle();
+            printf("Rectangle Modified!\n");
+        }
+        else if(choice == 8) {
+            printf("Exiting...\n");
+            break;
+        }
+        else {
+            printf("Invalid Choice!\n");
+        }
     }
-return 0;
+
+    return 0;
 }
